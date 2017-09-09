e9ac9dff52ebfa4fb32f3b39fc8f430a6ab84567
LabutinIgor <Labutin.IgorL@gmail.com>
2017-09-09 20:42:21 +0300

    Implemented Lexer

--- src/main/scala/ru/spbau/jvm/scala/task01_calculator/Main.scala
+++ src/main/scala/ru/spbau/jvm/scala/task01_calculator/Main.scala
@@ -1,12 +1,16 @@
 package ru.spbau.jvm.scala.task01_calculator
 
-import ru.spbau.jvm.scala.task01_calculator.parsing.{Lexeme, LexemeType, Lexer, Parser}
+import ru.spbau.jvm.scala.task01_calculator.parsing.{Lexer, Parser}
 
 object Main {
 
   def main(args: Array[String]): Unit = {
     for (line <- io.Source.stdin.getLines) {
       if (line.equals("exit")) System.exit(0)
+
+      val list = Lexer.parse(line)
+      list.foreach(x => println(x.lexeme_type + " " + x.lexeme_value))
+
       val expression = Parser.parse(Lexer.parse(line))
       println(expression.eval())
     }
--- src/main/scala/ru/spbau/jvm/scala/task01_calculator/parsing/Lexer.scala
+++ src/main/scala/ru/spbau/jvm/scala/task01_calculator/parsing/Lexer.scala
@@ -1,7 +1,46 @@
 package ru.spbau.jvm.scala.task01_calculator.parsing
 
 object Lexer {
+  var currentPos = 0
+
   def parse(line: String): List[Lexeme] = {
-    List()
+    currentPos = 0
+    var lexems: List[Lexeme] = List[Lexeme]()
+    while (currentPos < line.length) {
+      if (line(currentPos) != ' ') {
+        lexems :+= (line(currentPos) match {
+          case '(' | ')' => new Lexeme(LexemeType.Bracket, "" + line(currentPos))
+          case ',' => new Lexeme(LexemeType.Comma, ",")
+          case c if c >= '0' && c <= '9' => new Lexeme(LexemeType.Const, parseConst(line))
+          case c if c >= 'a' && c <= 'z' => new Lexeme(LexemeType.FunctionName, parseFunctionName(line))
+          case _ if currentPos == 0 || line(currentPos - 1) == '(' => new Lexeme(LexemeType.UnOp, "" + line(currentPos))
+          case _ => new Lexeme(LexemeType.BinOp, "" + line(currentPos))
+        })
+      }
+      currentPos += 1
+    }
+    lexems
+  }
+
+  private def parseConst(line: String): String = {
+    var res = ""
+    while (currentPos < line.length &&
+      (line(currentPos) == '.' || line(currentPos) >= '0' && line(currentPos) <= '9')) {
+
+      res += line(currentPos)
+      currentPos += 1
+    }
+    currentPos -= 1
+    res
+  }
+
+  private def parseFunctionName(line: String): _root_.scala.Predef.String = {
+    var res = ""
+    while (currentPos < line.length && line(currentPos) >= 'a' && line(currentPos) <= 'z') {
+      res += line(currentPos)
+      currentPos += 1
+    }
+    currentPos -= 1
+    res
   }
 }
